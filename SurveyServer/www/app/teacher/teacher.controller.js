de665ef81d07490b8e7701deb05ac5d5d3ad47bd
ChristianCuBu <christiancuaresma@gmail.com>
2016-06-01 17:10:57 +0200

    teacher view start v2

---
+++ SurveyServer/www/app/teacher/teacher.controller.js
@@ -0,0 +1,13 @@
+/**
+ * Created by ns on 01/06/16.
+ */
+angular.module('teacher');
+angular.controller('teacherController',teacherController);
+
+function teacherController($http, rsaFunctions, bigInt){
+
+    var vm = this;
+
+
+
+}
